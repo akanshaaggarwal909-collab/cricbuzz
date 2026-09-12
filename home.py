3a436bd8bfc13af9133c16e06181e719f5158b67
akanshaaggarwal909-collab <akanshaaggarwal909@gmail.com>
2026-09-12 18:12:39 +0530

    Update home.py

--- home.py
+++ home.py
@@ -8,9 +8,9 @@ st.set_page_config(page_title="Home | Cricbuzz LiveStats", page_icon="🏠", lay
 
 st.title("🏠 Home")
 if st.button("Initialize Database"):
-   from utils.db_connection
-   import init_db init_db()
-   st.success("Database initialized!") 
+   from utils.db_connection import init_db
+   init_db()
+   st.success("Database initialized!")
 st.markdown("""
 ## About This Project
 
