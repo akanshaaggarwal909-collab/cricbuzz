cb2e96087f549787d4f82c3ca5f9c6a1b9ccd3ef
akanshaaggarwal909-collab <akanshaaggarwal909@gmail.com>
2026-09-12 20:03:46 +0530

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -7,6 +7,9 @@ import streamlit as st
 st.set_page_config(page_title="Home | Cricbuzz LiveStats", page_icon="🏠", layout="wide")
 
 st.title("🏠 Home")
+    if st.button("Initialise database"):
+       from utils.db_connection import init_db init_db()
+       st.success("Database initialized!") 
 
 st.markdown("""
 ## About This Project
